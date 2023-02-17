f6f53f7636b1d7feee8a70dbb11bc3533679b3fe
ming-taro <hera002008@naver.com>
2023-02-18 07:57:53 +0900

    [ADD] 주문 완료페이지에서 메인화면으로 돌아가는 버튼 생성

--- app/src/main/java/com/example/android/Card2Activity.java
+++ app/src/main/java/com/example/android/Card2Activity.java
@@ -2,7 +2,11 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Card2Activity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class Card2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card2);
+
+        ImageView imageView = findViewById(R.id.card_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Card2Activity.this,Review2Activity.class));
+            }
+        });
+        Button card_buy = findViewById(R.id.card_buy);
+        card_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Card2Activity.this, OrderCompleteActivity.class));
+            }
+        });
+        Button card_deny = findViewById(R.id.card_deny);
+        card_deny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
-}
+    }
