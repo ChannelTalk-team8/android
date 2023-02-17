64a347adee8892543eeb6fc9a0acf5189167a9a3
ming-taro <hera002008@naver.com>
2023-02-18 06:48:57 +0900

    [ADD] 추천인 프로필 페이지의 리뷰칸에 별점 평가 추가

--- app/src/main/java/com/example/android/ReviewActivity.java
+++ app/src/main/java/com/example/android/ReviewActivity.java
@@ -3,12 +3,26 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommender_profile);
+
+        btnBack = findViewById(R.id.back_button);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
