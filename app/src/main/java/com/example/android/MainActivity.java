10ca4e3918fbc6e3a4e792d65bbfadad1e76a21b
ming-taro <hera002008@naver.com>
2023-02-18 02:46:32 +0900

    [ADD] 카테고리 선택시 화면크기, 가격, 세부 사항 입력칸 추가

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.chip.Chip;
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
     private ChipGroup purposeChipGroup;
     private ChipGroup weightChipGroup;
+    private ChipGroup priceChipGroup;
+    private ChipGroup sizeChipGroup;
+
+    private Button btnSearch;
+
+    private String selectedPurpose = null;
+    private String selectedWeight = null;
+    private String selectedPrice = null;
+    private String selectedSize = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         purposeChipGroup = (ChipGroup) findViewById(R.id.purpose_chip_group);
         weightChipGroup = (ChipGroup) findViewById(R.id.weight_chip_group);
+        priceChipGroup = (ChipGroup) findViewById(R.id.price_chip_group);
+        sizeChipGroup = (ChipGroup) findViewById(R.id.size_chip_group);
+
+        btnSearch = findViewById(R.id.button_search);
 
         ArrayList<String> purpose = new ArrayList<>();
         purpose.add("사무/인강용");
@@ -39,36 +53,115 @@ public class MainActivity extends AppCompatActivity {
             chip.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    System.out.println("purpose = " + chip.getText().toString());
-//                    Toast.makeText(getApplicationContext(), "Check", Toast.LENGTH_SHORT).show();
+                    if(chip.isChecked()){
+                        selectedPurpose = chip.getText().toString();
+                    }
+                    else{
+                        selectedPurpose = null;
+                    }
+                }
+            });
+        }
+
+        ArrayList<String> weight = new ArrayList<>();
+        weight.add("1kg 미만");
+        weight.add("1.0~1.6kg");
+        weight.add("1.7~2.0kg");
+        weight.add("2.0kg~2.5kg");
+        weight.add("2.0kg~2.5kg");
+        weight.add("2.6~3.0kg");
+        weight.add("3kg 이상");
+
+        for(int i = 0; i<weight.size(); i++){
+            Chip chip = new Chip(this);
+            chip.setText(weight.get(i));
+            chip.setCheckable(true);
+            chip.setCloseIconVisible(false);   
+            weightChipGroup.addView(chip);     
+
+            chip.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(chip.isChecked()){
+                        selectedWeight = chip.getText().toString();
+                    }
+                    else{
+                        selectedWeight = null;
+                    }
                 }
             });
         }
 
         ArrayList<String> price = new ArrayList<>();
-        price.add("1kg 미만");
-        price.add("1.0~1.6kg");
-        price.add("1.7~2.0kg");
-        price.add("2.0kg~2.5kg");
-        price.add("2.0kg~2.5kg");
-        price.add("2.6~3.0kg");
-        price.add("3kg 이상");
+        price.add("100만 미만");
+        price.add("100~150만");
+        price.add("150~200만");
+        price.add("200~250만");
+        price.add("250만~300만");
+        price.add("300만 초과");
 
-        for(int i = 0; i<purpose.size(); i++){
+        for(int i = 0; i<price.size(); i++){
             Chip chip = new Chip(this);
             chip.setText(price.get(i));
-            chip.setCheckable(true);             // 체크 표시 사용 여부
-            chip.setCloseIconVisible(false);     // close icon 표시 여부
-            weightChipGroup.addView(chip);             // chip group 에 해당 chip 추가
+            chip.setCheckable(true);
+            chip.setCloseIconVisible(false);
+            priceChipGroup.addView(chip);
 
             chip.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    System.out.println("purpose = " + chip.getText().toString());
-//                    Toast.makeText(getApplicationContext(), "Check", Toast.LENGTH_SHORT).show();
+                    if(chip.isChecked()){
+                        selectedPrice = chip.getText().toString();
+                    }
+                    else{
+                        selectedPrice = null;
+                    }
                 }
             });
         }
 
+        ArrayList<String> size = new ArrayList<>();
+        size.add("17인치");
+        size.add("16인치");
+        size.add("15인치");
+        size.add("14인치");
+        size.add("13인치");
+        size.add("그 외");
+
+        for(int i = 0; i<size.size(); i++){
+            Chip chip = new Chip(this);
+            chip.setText(size.get(i));
+            chip.setCheckable(true);
+            chip.setCloseIconVisible(false);
+            sizeChipGroup.addView(chip);
+
+            chip.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(chip.isChecked()){
+                        selectedSize = chip.getText().toString();
+                    }
+                    else{
+                        selectedSize = null;
+                    }
+                }
+            });
+        }
+        
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(selectedPurpose == null || selectedWeight == null || selectedPrice == null || selectedSize == null) {
+                    Toast.makeText(getApplicationContext(), "선택하지 않은 항목이 있습니다.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    System.out.println("purpose = " + selectedPurpose);
+                    System.out.println("price = " + selectedWeight);
+                    System.out.println("price = " + selectedPrice);
+                    System.out.println("size = " + selectedSize);
+                }
+            }
+        });
     }
+
 }
